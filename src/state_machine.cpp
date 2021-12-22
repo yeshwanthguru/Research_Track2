5be209bae16591e1f0009c153f2af2c53cbb1eb5
guru <yeshwanth445@gmail.com>
2021-12-22 17:06:01 +0100

    testing done in action server implementation updates to be done

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -1,75 +1,69 @@
 #include "ros/ros.h"
 #include "rt2_assignment1/Command.h"
-#include "rt2_assignment1/Position.h"
 #include "rt2_assignment1/RandomPosition.h"
-#include <actionlib/client/simple_action_client.h>
-#include <actionlib/client/terminal_state.h>
-#include <rt2_assignment1/RcAction.h>
+#include "rt2_assignment1/RcAction.h"
+#include "actionlib/client/simple_action_client.h"
+#include "actionlib/client/terminal_state.h"
 
-bool start = false; 
-bool goal_reached = false;
-bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res)
-{
-    if (req.command == "start")
-    {
-        start = true;
+bool start = false;
+
+
+bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res){
+    if (req.command == "start"){
+    	start = true;
     }
-    else
-    {
-        start = false;
+    else {
+    	start = false;
     }
     return true;
 }
 
-
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "state_machine");
-    ros::NodeHandle n;
-    ros::ServiceServer service = n.advertiseService("/user_interface", user_interface);
-    ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
-    actionlib::SimpleActionClient<rt2_assignment1::RcAction> ac("go_to_point", true);
+   ros::init(argc, argv, "state_machine");
+   ros::NodeHandle n;
+   ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
+   ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
+   actionlib::SimpleActionClient <rt2_assignment1::RcAction> pos("/go_to_point", true);
+   
+   rt2_assignment1::RcGoal gp;
+   rt2_assignment1::RandomPosition rp;
+   rp.request.x_max = 5.0;
+   rp.request.x_min = -5.0;
+   rp.request.y_max = 5.0;
+   rp.request.y_min = -5.0;
 
-    rt2_assignment1::RandomPosition rp;
-    rt2_assignment1::RcGoal goal;
+   while(ros::ok()){
+   	ros::spinOnce();
+   	if (start){
+   		client_rp.call(rp);
+        
+        gp.x=rp.response.x; 
+        gp.y=rp.response.y;  
+        gp.theta=rp.response.theta;
 
-    rp.request.x_max = 5.0;
-    rp.request.x_min = -5.0;
-    rp.request.y_max = 5.0;
-    rp.request.y_min = -5.0;
-
-    while(ros::ok())
-    {
-        ros::spinOnce();
-        if (start)
+        std::cout << "\nGoing to the position: x= " << gp.x << "\nGoing to the position: y= " <<gp.y << " \nGoing to the position:theta = " <<gp.theta << std::endl;
+        pos.sendGoal(gp);
+        
+        while (true)
         {
-            client_rp.call(rp);
-            goal.x = rp.response.x;
-            goal.y = rp.response.y;
-            goal.theta = rp.response.theta;
-
-            std::cout << "\nGoing to the position: x= " << goal.x << " y= " << goal.y << " theta = " << goal.theta << std::endl;
-            ac.sendGoal(goal);
-            while (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-            {
-                ros::spinOnce();
-                if (start == false)
-                {
-                  //cancel all the goals
-                  ac.cancelAllGoals();
-                  goal_reached=false;
-
-                  break;
+           ros::spinOnce();
+           if(start==false)
+           {
+                pos.cancelGoal();
+                    std::cout << "\n goal cancelled" << std::endl;
+                    break;
                 }
-                goal_reached=true;
+                else
+                {
+                    if (pos.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+                    {
+                        std::cout << "\nGoal Reached!" << std::endl;
+                        break;
+                    }
+                }
             }
         }
-
-    if(goal_reached)
-    {
-        std::cout << "\nPosition reached" << std::endl;
-        goal_reached = false;
     }
-    }
-  return 0 ;
+    return 0;
 }
