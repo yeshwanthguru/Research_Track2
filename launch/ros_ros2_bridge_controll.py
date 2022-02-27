86a9292c1f2e8716dbc2dd67776d8454d522c78f
yeshwanthguru <72270080+yeshwanthguru@users.noreply.github.com>
2022-02-27 09:07:30 +0100

    Update ros_ros2_bridge_controll.py
    launch file update for the nodes from talker example of slides and updated from docker image to github

--- launch/ros_ros2_bridge_controll.py
+++ launch/ros_ros2_bridge_controll.py
@@ -4,19 +4,19 @@ from launch_ros.descriptions import ComposableNode
 def generate_launch_description():
   """Generate launch description with multiple components."""
   container = ComposableNodeContainer(
-      name='my_container',
+      name='rt2_assignment1_ctnr',
       namespace='',
       package='rclcpp_components',
       executable='component_container',
       composable_node_descriptions=[
         ComposableNode(
-          package='composition',
-          plugin='composition::Talker',
-          name='talker'),
+          package='rt2_assignment1',
+          plugin='rt2_assignment1::Roboran_Pos_Service',
+          name='ros2_position_server_node'),
         ComposableNode(
-          package='composition',
-          plugin='composition::Listener',
-          name='listener')
+          package='rt2_assignment1',
+          plugin='rt2_assignment1::Fin_State_Machi',
+          name='ros2_state_machine_node')
       ],
       output='screen',
 )
